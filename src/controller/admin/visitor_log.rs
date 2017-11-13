21abdd6cb549eb97b4d0f27c08a0fea541bd3012
Samray Leung <samrayleung@gmail.com>
2017-11-13 17:05:51 +0800

    fix sql bug. add daily and monthly user view in dashboard

--- src/controller/admin/visitor_log.rs
+++ src/controller/admin/visitor_log.rs
@@ -15,3 +15,16 @@ pub fn count_monthly_page_view(db: DB) -> Json<Vec<(NaiveDateTime, i64)>> {
     let results = VisitorLog::count_monthly_page_view(db.conn());
     Json(results)
 }
+
+
+#[get("/admin/log/daily_uv")]
+pub fn count_daily_user_view(db: DB) -> Json<Vec<(NaiveDateTime, i64)>> {
+    let results = VisitorLog::count_daily_user_view(db.conn());
+    Json(results)
+}
+
+#[get("/admin/log/monthly_uv")]
+pub fn count_monthly_user_view(db: DB) -> Json<Vec<(NaiveDateTime, i64)>> {
+    let results = VisitorLog::count_monthly_user_view(db.conn());
+    Json(results)
+}
