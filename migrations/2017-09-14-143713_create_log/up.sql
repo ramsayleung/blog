942360a1dad4363a22b77f34aa86db7a4e3e066b
samrayleung <samrayleung@gmail.com>
2019-03-29 19:26:16 +0800

    add alter git command

--- migrations/2017-09-14-143713_create_log/up.sql
+++ migrations/2017-09-14-143713_create_log/up.sql
@@ -1,6 +1,6 @@
 -- Your SQL goes here
 CREATE TABLE public.visitor_log (
-id integer NOT NULL,
+id serial PRIMARY KEY,
 ip inet NOT NULL,
 access_time timestamp without time zone NOT NULL,
 user_id integer DEFAULT 0 NOT NULL
--- migrations/20170723104134_create_post/up.sql
+++ migrations/20170723104134_create_post/up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE post (
-id integer NOT NULL,
-title character varying NOT NULL,
-subtitle character varying NOT NULL,
+id serial PRIMARY KEY,
+title varchar NOT NULL,
+subtitle varchar NOT NULL,
 raw_content text NOT NULL,
 rendered_content text NOT NULL,
 create_time timestamp without time zone NOT NULL,
@@ -9,6 +9,6 @@ modify_time timestamp without time zone NOT NULL,
 post_type integer NOT NULL,
 hit_time integer NOT NULL,
 published boolean DEFAULT false NOT NULL,
-slug_url character varying NOT NULL,
+slug_url varchar NOT NULL,
 enable_comment boolean DEFAULT true NOT NULL
 );
